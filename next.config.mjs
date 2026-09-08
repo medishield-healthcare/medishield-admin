67753e90f60ff5fd854cc3b00f0992fe0a5b246f
KabirHas <kabir.infoseek@gmail.com>
2026-09-08 14:56:25 +0530

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'images1.dentalkart.com',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'dentalkart-application-media.s3.ap-south-1.amazonaws.com',
-                port: '',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: '**',
-                port: '',
-                pathname: '**',
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images1.dentalkart.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "dentalkart-application-media.s3.ap-south-1.amazonaws.com",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+        port: "",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "localhost",
+        port: "8081",
+        pathname: "**",
+      },
+      {
+        protocol: 'https',
+        hostname: 'medisheild-react-native-expo.vercel.app',
+        port: '',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
